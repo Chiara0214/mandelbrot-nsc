0544433acc1d78b053fc984bf4a15d00d54d902e
Chiara0214 <chiara02.caselli@edu.unife.it>
2026-02-05 15:02:23 +0100

    added computation timer, refactored compute_mandelbrot to accept region and resolution parameters

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,7 +3,9 @@ Mandelbrot Set Generator
 Author : Chiara Caselli
 Course : Numerical Scientific Computing 2026
 """
+from tracemalloc import start
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 
 def mandelbrot_point(c):
@@ -30,20 +32,26 @@ def mandelbrot_point(c):
 
     return max_iter
 
-def compute_mandelbrot():
+def compute_mandelbrot(x_min=-2.0, x_max=1.0, y_min=-1.5, y_max=1.5, width=1024, height=1024):
     """
-    Function that returns a list with the number of iterations for each point
+    Function that returns a list with the number of iterations for each point given a region and a resolution
+
+    Parameters
+    ----------
+    x_min : float
+    x_max : float
+    y_min : float
+    y_max : float
+    width : int
+    height : int
 
     Returns
     -------
     list of int
-        Output value
     """
-    x_min, x_max = -2.0, 1.0
-    y_min, y_max = -1.5, 1.5
 
-    x_values = np.linspace(x_min, x_max, 100)
-    y_values = np.linspace(y_min, y_max, 100)
+    x_values = np.linspace(x_min, x_max, width)
+    y_values = np.linspace(y_min, y_max, height)
     n_iterations = []
 
     for x in x_values:
@@ -58,6 +66,12 @@ if __name__ == "__main__":
     #testing the function, returns 100 = max_iter
     print(mandelbrot_point(0))
 
+    start = time.time()
+
     n_iterations = compute_mandelbrot()
+
+    elapsed = time.time() - start
+    print(f"Computation took {elapsed:.3f} seconds")
+
 
         
