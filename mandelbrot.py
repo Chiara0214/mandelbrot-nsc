fa6ade3a3d49dec969930b48e9fb9232815bc6fc
Chiara0214 <chiara02.caselli@edu.unife.it>
2026-02-05 14:52:04 +0100

    added mandelbrot_point function for computing the max number of iterations given a complex number

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,19 +3,33 @@ Mandelbrot Set Generator
 Author : Chiara Caselli
 Course : Numerical Scientific Computing 2026
 """
-def f(x):
+
+def mandelbrot_point(c):
     """
-    Example function
+    Function that takes a complex number c as input and returns the number of iterations
 
     Parameters
     ----------
-    x : float
+    c : complex
         Input value
 
     Returns
     -------
-    float
+    int
         Output value
     """
-    # TODO : Implement the algorithm
-    pass
+    z_n = 0
+    max_iter = 100
+
+    for n in range(max_iter):
+        z_n = z_n**2 + c
+        if abs(z_n) > 2:
+            return n
+
+    return max_iter
+
+if __name__ == "__main__":
+
+    #testing the function, returns 100 = max_iter
+    print(mandelbrot_point(0))
+        
